46172b0de6bc5ec573eaf4bb75988bd25f532db9
DezChuang <dissaivent@gmail.com>
2019-10-14 22:58:45 +0800

    Init UI and data structure

---
+++ src/components/ToBuyListCard/index.vue
@@ -0,0 +1,36 @@
+<template>
+  <li class="cp-to-buy-list-card">
+    <div class="column-header">
+      <h4>{{title}}</h4>
+    </div>
+    <ul class="task-list"
+        :id="title">
+      <li class="task"
+          :key="item"
+          v-for="item in list">
+        <p>{{item}}</p>
+      </li>
+    </ul>
+  </li>
+</template>
+
+<script>
+  export default {
+    name: 'ToBuyListCard',
+    props: {
+      title: {
+        type: String,
+        required: true
+      },
+      list: {
+        type: Array,
+        required: true
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  .cp-to-buy-list-card {
+  }
+</style>
--- src/views/Buy/index.vue
+++ src/views/Buy/index.vue
@@ -1,18 +1,52 @@
 <template>
-  <el-card>
+  <div>
     <h2>待購清單</h2>
-
-  </el-card>
+    <el-card>
+      <ul class="columns">
+        <to-buy-list-card :key="item.id"
+                          v-for="item in cardList"
+                          :title="item.title"
+                          :list="item.list" />
+      </ul>
+    </el-card>
+  </div>
 </template>
 
 <script>
+  import dragula from 'dragula'
+  import ToBuyListCard from '@/components/ToBuyListCard'
   export default {
     name: 'Buy',
+    components: { ToBuyListCard },
     data() {
-      return {}
-    }
+      return {
+        cardList: [
+          {
+            id: 'want',
+            title: '想要',
+            list: ['iPhone 11 Pro Max', 'BOSE Lifestyle 600']
+          },
+          {
+            id: 'need',
+            title: '需要',
+            list: ['筆電背包', '眼鏡', '手錶']
+          },
+          {
+            id: 'satisfy',
+            title: '滿足',
+            list: ['空氣清淨機', '電磁爐', 'Brita 濾水壺']
+          },
+          {
+            id: 'sublimate',
+            title: '昇華',
+            list: ['任天堂 Switch', 'PS4', 'AirPods']
+          }
+        ]
+      }
+    },
+    mounted() {}
   }
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 </style>
